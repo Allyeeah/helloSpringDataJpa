7e52fd6adfa41cdae59f4ed983df963375db830b
daye <dbk1102@naver.com>
2025-06-05 23:36:27 +0900

    feat: User.java 생성

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/User.java
@@ -0,0 +1,26 @@
+package kr.ac.hansung.cse.hellospringdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+}
