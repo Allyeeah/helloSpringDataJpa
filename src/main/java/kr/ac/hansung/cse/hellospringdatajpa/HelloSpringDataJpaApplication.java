0a4431dfbf32f21c23e1ad912d6017f217fa67b8
daye <dbk1102@naver.com>
2025-06-06 11:30:56 +0900

    feat: database 생성 및 연동

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/HelloSpringDataJpaApplication.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/HelloSpringDataJpaApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloSpringDataJpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringDataJpaApplication.class, args);
     }
-
 }
+
